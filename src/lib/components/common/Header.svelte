68fcd44cbf01fcb3695d22dbb8f7b38eb213bdfd
samithiwat <samithiwat2544@gmail.com>
2022-02-02 03:19:01 +0700

    Create Header

---
+++ src/lib/components/common/Header.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import Icon from '@iconify/svelte';
+</script>
+
+<div class="bg-headerBg text-center font-MuseoModerno w-screen h-100">
+	<h2 class="text-white">Hello, Visitor!</h2>
+	<h5 class="text-white">Welcome to my personnal website</h5>
+	>
+	<span class="flex flex-row justify-center my-12 gap-28">
+		<a href="https://github.com/samithiwat" target="_blank" class="">
+			<Icon icon="akar-icons:github-fill" color="white" class="w-20 h-20" />
+		</a>
+		<a href="https://www.facebook.com/profile.php?id=100004633398479" target="_blank">
+			<Icon icon="el:facebook" color="white" class="w-20 h-20" />
+		</a>
+		<a href="https://www.instagram.com/samithiwat/" target="_blank">
+			<Icon icon="ant-design:instagram-filled" color="white" class="w-20 h-20" />
+		</a>
+	</span>
+	<h5 class="text-white text-lg">Follow me</h5>
+</div>
