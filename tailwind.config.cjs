e0013f19505e6d11aa6624f7fded431b0b4071f1
samithiwat <samithiwat2544@gmail.com>
2022-02-02 03:03:49 +0700

    Setup TailwindCSS

---
+++ tailwind.config.cjs
@@ -0,0 +1,33 @@
+module.exports = {
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	theme: {
+		extend: {
+			backgroundImage: {
+				headerBg: "url('/images/background/header-bg.png')"
+			},
+			colors: {
+				peach: {
+					1: '#f0e7e4'
+				},
+				black: {
+					1: '#131919'
+				},
+				carafe: {
+					1: '634f40'
+				},
+				nude: {
+					1: '#b49a87'
+				}
+			},
+			fontFamily: {
+				MuseoModerno: ['MuseoModerno', 'san-serif'],
+				Lato: ['Lato', 'sans-serif'],
+				Poppins: ['Poppins', 'sans-serif']
+			},
+			spacing: {
+				100: '25rem'
+			}
+		}
+	},
+	plugins: []
+};
