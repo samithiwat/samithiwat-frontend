12b624e96d9836e1294f53a99569ccd463d99093
samithiwat <samithiwat2544@gmail.com>
2022-02-02 03:16:09 +0700

    Change format of layout and landing page

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,45 +1,29 @@
-<script lang="ts">
-	import Header from '$lib/header/Header.svelte';
-	import '../app.css';
+<script context="module" lang="ts">
+	/** @type {import('sveltejs/kit').Load}*/
+	export async function load({ url }) {
+		return {
+			props: {
+				currentPath: url.pathname
+			}
+		};
+	}
 </script>
 
-<Header />
+<script lang="ts">
+	import { prefetchRoutes } from '$app/navigation';
+	import { onMount } from 'svelte';
+	import Nav from '$lib/components/common/Nav.svelte';
+	import '../app.css';
+	import { paths } from '../common/constant/path';
 
+	export let currentPath: string;
+
+	onMount(() => {
+		prefetchRoutes();
+	});
+</script>
+
+<Nav {paths} {currentPath} />
 <main>
 	<slot />
 </main>
-
-<footer>
-	<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
-</footer>
-
-<style>
-	main {
-		flex: 1;
-		display: flex;
-		flex-direction: column;
-		padding: 1rem;
-		width: 100%;
-		max-width: 1024px;
-		margin: 0 auto;
-		box-sizing: border-box;
-	}
-
-	footer {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		padding: 40px;
-	}
-
-	footer a {
-		font-weight: bold;
-	}
-
-	@media (min-width: 480px) {
-		footer {
-			padding: 40px 0;
-		}
-	}
-</style>
