805387fa1d16cf5d4b757ebd011be90406c35c84
samithiwat <samithiwat2544@gmail.com>
2022-02-10 23:55:53 +0700

    Update logic of arrow click

--- src/lib/components/landing/GithubRepositories.svelte
+++ src/lib/components/landing/GithubRepositories.svelte
@@ -1,44 +1,15 @@
 <script lang="ts">
 	import SmallPhotoCard from '../common/Card/SmallPhotoCard.svelte';
 	import Icon from '@iconify/svelte';
-	import type { Repository } from '$lib/common/types/github-repo';
-	import { leftArrowStore, rightArrowStore } from '$lib/stores/github-repository.store';
+	import {
+		displayRepositoriesCardStore,
+		leftArrowStore,
+		repositoriesStore,
+		rightArrowStore
+	} from '$lib/stores/github-repository.store';
 	import type { SmallPhotoCardProps } from '$lib/common/types/small-photo-card';
-
-	const repoes: Repository[] = [
-		{
-			name: 'svelte-icons',
-			author: 'svelte',
-			description: 'Svelte Icons',
-			url: '',
-			stars: 0,
-			createdAt: '2020-01-01'
-		},
-		{
-			name: 'Coconut Empire',
-			author: 'samithiwat',
-			description: 'Svelte Icons',
-			url: '',
-			stars: 0,
-			createdAt: '2020-01-01'
-		},
-		{
-			name: 'Samithiwat Backend',
-			author: 'samithiwat',
-			description: 'Svelte Icons',
-			url: '',
-			stars: 0,
-			createdAt: '2020-01-01'
-		},
-		{
-			name: 'Samithiwat',
-			author: 'samithiwat',
-			description: 'Svelte Icons',
-			url: '',
-			stars: 0,
-			createdAt: '2020-01-01'
-		}
-	];
+	import { onMount } from 'svelte';
+	import moment from 'moment';
 
 	const cardProps: SmallPhotoCardProps[] = [
 		{
@@ -56,25 +27,79 @@
 		{
 			cardClass: 'card small-card text-gray-secondary',
 			titleColor: 'text-gray-primary'
+		},
+		{
+			cardClass: 'card small-card text-gray-secondary',
+			titleColor: 'text-gray-primary'
+		},
+		{
+			cardClass: 'card small-card text-gray-secondary',
+			titleColor: 'text-gray-primary'
 		}
 	];
 
-	let prevPos = 0;
+	$: selectedPos = 0;
+	$: offsetPos = 0;
+
+	function handleNextPageClick() {
+		const prevPos = selectedPos;
+		selectedPos += 1;
+
+		if (selectedPos > 3) {
+			selectedPos = 3;
+			offsetPos += 1;
+
+			if (offsetPos > $repositoriesStore.length - 4) {
+				offsetPos = $repositoriesStore.length - 4;
+			}
+
+			displayRepositoriesCardStore.setRepository($repositoriesStore, offsetPos);
+		}
+
+		updateHighlight(prevPos, selectedPos);
+	}
+
+	function handlePrevPageClick() {
+		const prevPos = selectedPos;
+		selectedPos -= 1;
+
+		if (selectedPos < 0) {
+			selectedPos = 0;
+			offsetPos -= 1;
+
+			if (offsetPos < 0) {
+				offsetPos = 0;
+			}
+
+			displayRepositoriesCardStore.setRepository($repositoriesStore, offsetPos);
+		}
+
+		updateHighlight(prevPos, selectedPos);
+	}
 
 	function handleCardClick(e: CustomEvent) {
 		const pos = e.detail.pos;
-		if (pos === prevPos) {
+		if (pos === selectedPos) {
 			return;
 		}
 
+		updateHighlight(selectedPos, pos);
+
+		selectedPos = pos;
+	}
+
+	function updateHighlight(prevPos: number, pos: number) {
 		cardProps[prevPos].cardClass = 'card small-card text-gray-secondary';
 		cardProps[prevPos].titleColor = 'text-gray-primary';
 
 		cardProps[pos].cardClass = 'small-card bg-selected-card text-white';
 		cardProps[pos].titleColor = 'text-white';
-		prevPos = pos;
-		console.log(e.detail);
 	}
+
+	onMount(async () => {
+		await repositoriesStore.fetch();
+		await displayRepositoriesCardStore.setRepository($repositoriesStore, offsetPos);
+	});
 </script>
 
 <div class="my-20 flex justify-center font-Poppins">
@@ -95,65 +120,78 @@
 				class="h-[22rem] w-[22rem] rounded-4xl"
 			/>
 			<div class="flex flex-col gap-y-5">
-				<p class="text-6xl font-bold text-white">Samithiwat</p>
-				<p class="text-2xl text-white">Samithiwat</p>
-				<p class="mt-2 h-44 w-200 text-base text-gray-secondary">
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer dignissim est ac neque
-					iaculis, vitae blandit enim laoreet. Sed in ligula augue. Vivamus in scelerisque eros, a
-					imperdiet libero. Nulla facilisi. Morbi eu velit nisi. Fusce neque eros, congue sit amet
-					erat eu, hendrerit egestas risus. Phasellus ac odio ut ante mattis pretium. Proin sit amet
-					ipsum vel neque mollis aliquam. Nulla facilisi. Duis euismod mauris nunc, accumsan tempus
-					justo mollis id. Quisque sit amet lectus ac mauris accumsan imperdiet. Aliquam non
-					hendrerit erat. Sed dolor neque, dignissim id vulputate sit amet, tristique nec libero.
-					Donec ornare posuere velit, et aliquam erat cursus id. Morbi nec accumsan purus.
-				</p>
-				<span class="flex flex-row justify-between">
+				{#if $displayRepositoriesCardStore.length === 0}
+					<h2>{$displayRepositoriesCardStore}</h2>
+				{:else}
+					<p class="text-6xl font-bold text-white">
+						{$displayRepositoriesCardStore[selectedPos].name}
+					</p>
+					<span class="flex items-center gap-x-1">
+						<p class="text-2xl text-white">{$displayRepositoriesCardStore[selectedPos].author}</p>
+					</span>
+					<p class="mt-2 h-32 w-200 text-base text-gray-secondary">
+						{$displayRepositoriesCardStore[selectedPos].description}
+					</p>
+				{/if}
+				<span class="flex w-[42rem] flex-row justify-between">
 					<span
 						class="flex flex-row items-center justify-center gap-x-1 text-xl text-gray-secondary"
 					>
-						<Icon icon="ant-design:calendar-outlined" class="h-5 w-5" />
-						23 Jan 2022
-						<Icon icon="ant-design:clock-circle-outlined" class="h-5 w-5" />
-						1:00 PM
+						Latest updated at
+
+						{moment($displayRepositoriesCardStore[selectedPos].updatedAt).format(
+							'ddd, Do MMM YYYY,'
+						)}
+						{$displayRepositoriesCardStore[selectedPos].time}
 					</span>
-					<button class="btn btn-white flex h-12 w-36 items-center justify-center gap-x-3">
+					<button
+						class="btn btn-white flex h-12 w-36 items-center justify-center gap-x-3"
+						on:click={() => {
+							window.open($displayRepositoriesCardStore[selectedPos].url);
+						}}
+					>
 						Browse <Icon icon="bx:bx-link-external" /></button
 					>
 				</span>
 			</div>
 		</div>
 		<div class="mt-4 flex flex-row items-center gap-x-4 ">
-			{#if repoes.length >= 4}
-				<div
-					on:click={() => {
-						leftArrowStore.triggle();
-					}}
-				>
-					<Icon icon={$leftArrowStore} class="h-12 w-12 text-yellow-card" />
-				</div>
+			<div
+				on:click={() => {
+					leftArrowStore.triggle();
+					handlePrevPageClick();
+				}}
+			>
+				<Icon icon={$leftArrowStore} class="h-12 w-12 text-yellow-card" />
+			</div>
+
+			{#if $displayRepositoriesCardStore.length === 0}
+				<h1>Empty</h1>
+			{:else}
+				{#each $displayRepositoriesCardStore as repo, pos}
+					<SmallPhotoCard
+						{pos}
+						title={repo.name}
+						desc={repo.author}
+						date={repo.updatedAt}
+						time={repo.time}
+						height={400}
+						width={285}
+						cardClass={cardProps[pos].cardClass}
+						titleColor={cardProps[pos].titleColor}
+						on:click={handleCardClick}
+					/>
+				{/each}
 			{/if}
-			{#each repoes as repo, pos}
-				<SmallPhotoCard
-					{pos}
-					title={repo.name}
-					desc={repo.author}
-					date={repo.createdAt}
-					height={380}
-					width={285}
-					cardClass={cardProps[pos].cardClass}
-					titleColor={cardProps[pos].titleColor}
-					on:click={handleCardClick}
-				/>
-			{/each}
-			{#if repoes.length >= 4}
-				<div
-					on:click={() => {
-						rightArrowStore.triggle();
-					}}
-				>
-					<Icon icon={$rightArrowStore} class="h-12 w-12 text-yellow-card" />
-				</div>
-			{/if}
+
+			<div
+				on:click={() => {
+					rightArrowStore.triggle();
+					handleNextPageClick();
+				}}
+			>
+				<Icon icon={$rightArrowStore} class="h-12 w-12 text-yellow-card" />
+			</div>
 		</div>
 	</div>
 </div>
