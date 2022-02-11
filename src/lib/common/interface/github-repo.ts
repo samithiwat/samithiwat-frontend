c0c48b3ea281e5db508d55e672698e4a8528ccc7
samithiwat <samithiwat2544@gmail.com>
2022-02-11 12:23:07 +0700

    Add field date and id in github-repo interface

--- src/lib/common/interface/github-repo.ts
+++ src/lib/common/interface/github-repo.ts
@@ -1,9 +1,11 @@
 export interface Repository {
+	id: number;
 	name: string;
 	author: string;
 	description: string;
 	url: string;
 	updatedAt: string;
+	date: string;
 	time: string;
 	stars: number;
 }
