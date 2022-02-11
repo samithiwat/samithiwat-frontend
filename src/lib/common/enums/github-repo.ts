1a0b59fad442efb0dcfecfdeca817b4ec4b2e020
samithiwat <samithiwat2544@gmail.com>
2022-02-11 12:21:03 +0700

    Add enum of github-repo

---
+++ src/lib/common/enums/github-repo.ts
@@ -0,0 +1,6 @@
+export enum GithubClassName {
+	CARD_SELECTED = 'small-card bg-selected-card text-white',
+	CARD_DESELECTED = 'card small-card text-gray-secondary',
+	TITLE_DESELECTED = 'text-gray-primary',
+	TITLE_SELECTED = 'text-white'
+}
