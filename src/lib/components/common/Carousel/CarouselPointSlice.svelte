26aa5e28365184cd1c6912417cd0e33561f5254b
samithiwat <samithiwat2544@gmail.com>
2022-02-12 11:45:13 +0700

    Create Carousel Point

---
+++ src/lib/components/common/Carousel/CarouselPointSlice.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import CarouselPoint from './CarouselPoint.svelte';
+
+	export let images = [];
+	export let width = 40;
+	let selectedPos = 0;
+</script>
+
+<div class="absolute flex h-full w-full flex-row items-end justify-center gap-x-6 py-3">
+	{#each images as image, pos}
+		{#key selectedPos}
+			<CarouselPoint {width} />
+		{/key}
+	{/each}
+</div>
