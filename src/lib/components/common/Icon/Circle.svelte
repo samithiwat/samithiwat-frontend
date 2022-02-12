5bee63fdf7f01616832f1ad633fcf809ea817310
samithiwat <samithiwat2544@gmail.com>
2022-02-12 11:40:29 +0700

    Add Card in timeline page

--- src/lib/components/common/Icon/Circle.svelte
+++ src/lib/components/common/Icon/Circle.svelte
@@ -2,6 +2,8 @@
 	export let color = 'bg-white';
 </script>
 
-<div class={'flex h-36 w-36 items-center justify-center rounded-full ' + color}>
+<div
+	class={'flex h-36 w-36 items-center justify-center rounded-full hover:animate-spin-slow ' + color}
+>
 	<slot />
 </div>
