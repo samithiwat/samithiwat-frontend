0af96e3d02a87d78ce727967cb22c88893a64079
samithiwat <samithiwat2544@gmail.com>
2022-02-12 11:42:29 +0700

    Add Modal in layout

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -15,6 +15,9 @@
 	import Nav from '$lib/components/common/Navbar/Nav.svelte';
 	import '../app.css';
 	import { paths } from '$lib/common/constant/path';
+	import Modal from '$lib/components/common/Modal/Modal.svelte';
+	import { modalPropsStore } from '$lib/stores/modal.store';
+	import Carousel from '$lib/components/common/Carousel/Carousel.svelte';
 
 	export let currentPath: string;
 
@@ -22,6 +25,21 @@
 		prefetchRoutes();
 	});
 </script>
+
+<Modal {...$modalPropsStore}>
+	<Carousel />
+	<div class="h-1/3 w-4/5 items-center font-Poppins text-lg text-white ">
+		<p>
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi euismod sem at lectus feugiat
+			aliquam. Nam sodales eleifend fringilla. Aliquam non nunc viverra, suscipit dui nec, faucibus
+			arcu. Donec porttitor libero congue dictum porttitor. Etiam eu pulvinar erat, at consectetur
+			quam. Nunc ultricies vestibulum turpis id varius.
+		</p>
+	</div>
+	<div class="flex w-full justify-end px-20">
+		<button class="btn btn-white h-12 w-36">Read more</button>
+	</div>
+</Modal>
 
 <Nav {paths} {currentPath} />
 <main>
