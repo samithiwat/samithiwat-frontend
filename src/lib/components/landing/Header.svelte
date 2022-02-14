b7039e6129b416ac0b52a46f9413e11394916110
samithiwat <samithiwat2544@gmail.com>
2022-02-14 11:49:13 +0700

    Add more responsive of header component

--- src/lib/components/landing/Header.svelte
+++ src/lib/components/landing/Header.svelte
@@ -3,7 +3,7 @@
 
 <div class="my-20 flex select-none items-center justify-center">
 	<div
-		class="mobile-m::text-3xl flex h-[32rem] w-9/12 flex-col items-center justify-center rounded-[2.5rem] bg-gray-primary font-Poppins text-2xl shadow-lg shadow-blue-primary transition duration-1000 ease-in-out hover:shadow-red-gradian sm:h-100 md:text-4xl lg:h-200 lg:text-6xl"
+		class="flex h-[32rem] w-9/12 flex-col items-center justify-center rounded-lg bg-gray-primary font-Poppins text-2xl shadow-lg shadow-blue-primary transition duration-1000 ease-in-out hover:shadow-red-gradian mobile-m:text-3xl sm:h-100 sm:rounded-[2.5rem] md:text-4xl lg:h-200 lg:text-6xl"
 	>
 		<div
 			class="z-10 flex flex-col items-center bg-gradient-to-r from-mint-gradian via-purple-gradian  to-red-gradian bg-clip-text font-bold text-transparent transition"
