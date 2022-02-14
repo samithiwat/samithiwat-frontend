b60c1e92c93a3de9ad4196e9983b13f061b75bdd
samithiwat <samithiwat2544@gmail.com>
2022-02-14 08:40:55 +0700

    Refactor aboutme

--- src/lib/components/landing/AboutMe.svelte
+++ src/lib/components/landing/AboutMe.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
+	import { aboutmeProps } from '$lib/stores/aboutme.store';
+
 	import Icon from '@iconify/svelte';
 </script>
 
 <div class="flex items-center justify-center">
 	<div
-		class="flex h-200 w-350 flex-col rounded-[2.5rem] bg-gray-primary p-32 font-Poppins text-6xl"
+		class="flex h-200 w-350 flex-col rounded-[2.5rem] bg-gray-primary p-32 font-Poppins text-6xl shadow-lg shadow-mint-primary transition duration-1000 hover:shadow-yellow-200"
 	>
 		<span class="flex select-none">
 			<p
@@ -16,30 +18,25 @@
 		<hr class="w-200 rounded-xl border-2 bg-gray-secondary" />
 		<div class="my-12 flex flex-row gap-10 font-Poppins">
 			<div class="flex flex-col items-center">
-				<div
-					class="my-5 h-100 w-100 rounded-2xl border-[18px] border-yellow-card bg-about-me bg-cover bg-center"
+				<img
+					src={$aboutmeProps.imgUrl}
+					alt="Samithiwat Pic"
+					class="my-5 h-100 w-100 rounded-2xl border-[18px] border-yellow-card bg-about-me bg-cover bg-center object-cover"
 				/>
 				<span class="flex select-none flex-row gap-x-2 text-white">
 					<Icon icon="cil:birthday-cake" class=" h-5 w-5" />
 					<p class="text-base">27 June 2001</p>
 				</span>
 			</div>
-			<div class="flex w-[45rem] flex-col gap-10">
+			<div class="flex w-[45rem] flex-col gap-5">
 				<span>
-					<p class="text-5xl font-bold text-white">Samithiwat Boonchai</p>
-					<p class="text-2xl font-semibold text-white">Software Engineer, Backend & Devops</p>
+					<p class="text-5xl font-bold text-white">{$aboutmeProps.name}</p>
+					<p class="text-2xl font-semibold text-white">{$aboutmeProps.desc}</p>
 				</span>
-				<p class="text-base text-gray-secondary ">
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer dignissim est ac neque
-					iaculis, vitae blandit enim laoreet. Sed in ligula augue. Vivamus in scelerisque eros, a
-					imperdiet libero. Nulla facilisi. Morbi eu velit nisi. Fusce neque eros, congue sit amet
-					erat eu, hendrerit egestas risus. Phasellus ac odio ut ante mattis pretium. Proin sit amet
-					ipsum vel neque mollis aliquam. Nulla facilisi. Duis euismod mauris nunc, accumsan tempus
-					justo mollis id. Quisque sit amet lectus ac mauris accumsan imperdiet. Aliquam non
-					hendrerit erat. Sed dolor neque, dignissim id vulputate sit amet, tristique nec libero.
-					Donec ornare posuere velit, et aliquam erat cursus id. Morbi nec accumsan purus.
+				<p class="h-3/5 text-base text-gray-secondary">
+					{$aboutmeProps.content}
 				</p>
-				<div class="my-3 flex flex-row items-end justify-end">
+				<div class=" flex flex-row items-end justify-end">
 					<button
 						class="btn btn-white flex h-10 w-40 flex-row items-center justify-center gap-x-2 text-base"
 					>
---
+++ src/lib/stores/aboutme.store.ts
@@ -0,0 +1,29 @@
+import type { AboutMeProps } from '$lib/common/types/aboutme';
+import { writable } from 'svelte/store';
+
+const createAboutMeProps = () => {
+	const { subscribe, set, update } = writable<AboutMeProps>({
+		name: 'Samithiwat Boonchai',
+		desc: 'Software Engineer, Backend & Devops',
+		content:
+			"Greetings! my name is Samithiwat Boonchai, a 2nd-year Computer Engineering student, at Chulalongkorn University. I'm a backend and DevOps, mostly backend stuff, ambitious software engineering. I love to collaborate with others. At this time I'm looking for a part-time job please contact me if you are interesting I hope experience and skill are exactly what you are looking for. looking forward to meet you!",
+		imgUrl: 'https://storage.googleapis.com/samithiwat-bucket/about-me-protrait.png'
+	});
+
+	const fetch = async () => {
+		set({
+			name: '',
+			desc: '',
+			content: '',
+			imgUrl: ''
+		});
+	};
+
+	return {
+		subscribe,
+		fetch,
+		update
+	};
+};
+
+export const aboutmeProps = createAboutMeProps();
