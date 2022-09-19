61a1c02b89ba60af052a22d7a78157e7620fd93a
samithiwat <samithiwat2544@gmail.com>
2022-09-20 00:33:00 +0700

    Remove unused repo

--- src/lib/api/github.ts
+++ src/lib/api/github.ts
@@ -8,22 +8,6 @@ import moment from 'moment';
 const mockData = [
 	{
 		framework: {
-			id: 1,
-			name: 'NONE',
-			color: 'fff' as Color,
-			iconName: '-',
-			iconColor: 'white' as Color
-		},
-		language: {
-			id: 1,
-			name: 'Go',
-			color: '00ADD8' as Color,
-			iconName: 'GO',
-			iconColor: 'white' as Color
-		}
-	},
-	{
-		framework: {
 			id: 2,
 			name: 'NONE',
 			color: 'fff' as Color,
